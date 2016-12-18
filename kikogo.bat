6a28d63652f37fb6884f0826feb24ecf2cb21dd2
FlorentGuedeau <florent.guedeau@gmail.com>
2016-12-18 12:06:08 +0100

    Add files via upload

---
+++ kikogo.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Bienvenue dans kikoflo !
+
+REM npm update = installation ou màj des node_modules, nodevars = initialisation de l'environnement, grunt = lancement de la compilation et du watch
+
+npm update & nodevars & grunt
