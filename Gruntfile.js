7f9fc2a6f0991982368567b97ba817105a2b47b9
FlorentGuedeau <florent.guedeau@gmail.com>
2016-12-28 20:17:52 +0100

    Mise en place de CSS Comb

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,16 @@ module.exports = function(grunt) {
         watch: {
             sass: {
                 files: ['sass/**/*.{scss,sass}'],
-                tasks: ['sass:dist', 'postcss:dist']
+                tasks: ['sass:dist', 'csscomb:dist', 'postcss:dist']
             },
             livereload: {
-                files: ['html/*.html', 'html/*.php', 'html/javascripts/**/*.{js,json}', 'html/stylesheets/*.css','html/images/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                files: ['html/*.html', 'html/*.php', 'html/javascripts/**/*.{js,json}', 'html/stylesheets/*.css','html/images/**/*.{png,jpg,jpeg,gif}'],
                 options: {
-                    livereload: true
+                    livereload: true,
+                    livereload: {
+                        host: 'localhost',
+                        port: 17700,
+                    }
                 }
             }
         },
@@ -46,11 +50,19 @@ module.exports = function(grunt) {
                 }]
             }
         },
+        csscomb: {
+            dist: {
+                files: {
+                    'html/stylesheets/main.css': ['html/stylesheets/main.css']
+                }
+            }
+        },
     });
 
+    grunt.loadNpmTasks('grunt-csscomb'); // https://github.com/csscomb/grunt-csscomb
     grunt.loadNpmTasks('grunt-contrib-sass'); // https://github.com/gruntjs/grunt-contrib-sass
     grunt.loadNpmTasks('grunt-postcss'); // https://github.com/postcss/autoprefixer
     grunt.loadNpmTasks('grunt-contrib-imagemin'); // https://github.com/gruntjs/grunt-contrib-imagemin
     grunt.loadNpmTasks('grunt-contrib-watch'); // https://github.com/gruntjs/grunt-contrib-watch /// http://blog.grayghostvisuals.com/grunt/image-optimization/
-    grunt.registerTask('default', ['sass:dist', 'postcss:dist', 'imagemin', 'watch']);
+    grunt.registerTask('default', ['sass:dist', 'csscomb:dist', 'postcss:dist', 'imagemin', 'watch']);
 };
