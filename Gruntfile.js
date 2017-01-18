1f9e983e3c22dfd7ef6c6b749cebaad9f9c11739
FlorentGuedeau <florent.guedeau@gmail.com>
2017-01-18 18:43:02 +0100

    Add files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,27 +5,25 @@ module.exports = function(grunt) {
                 files: ['sass/**/*.{scss,sass}'],
                 tasks: ['sass:dist', 'postcss:dist']
             },
-            coffee: {
-                files: ['coffeescripts/**/*.coffee'],
-                tasks: ['coffee']
-            },
-            livereload: {
-                files: ['html/*.html', 'html/*.php', 'html/javascripts/**/*.{js,json}', 'html/stylesheets/*.css','html/images/**/*.{png,jpg,jpeg,gif,webp,svg}'],
-                options: {
-                    livereload: true 
-                }
-            }
+            //livereload: {
+            //    files: ['html/*.html', 'html/*.php', 'html/javascripts/**/*.{js,json}', 'html/stylesheets/*.css','html/images/**/*.{png,jpg,jpeg,gif}'],
+            //    options: {
+            //        livereload: true,
+            //        livereload: {
+            //            host: 'localhost',
+            //            port: 17700,
+            //        }
+            //    }
+            //}
         },
         sass: {                              // Task
             dist: {                            // Target
                 options: {                       // Target options
-                    style: 'nested',
+                    //style: 'compressed',
                     compass: true
                 },
                 files: {                         // Dictionary of files
                     'html/stylesheets/main.css': 'sass/main.scss',       // 'destination': 'source',
-                    'html/stylesheets/ie9.css': 'sass/ie9.scss',       // 'destination': 'source'
-                    'html/stylesheets/editor-style.css': 'sass/editor-style.scss'       // 'destination': 'source'
                 }
             }
         },
@@ -39,28 +37,44 @@ module.exports = function(grunt) {
                 ]
             },
             dist: {
-                src: 'html/stylesheets/*.css'
+                //src: 'html/stylesheets/*.css',
+                src: 'html/stylesheets/main.css'
             }
         },
-        coffee: {
-            compile: {
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
                 files: [{
-                    expand: true,
-                    cwd: 'coffeescripts',
-                    src: '{,*/}*.{coffee,litcoffee,coffee.md}',
-                    dest: 'html/javascripts',
-                    ext: '.js'}
-                       ]
-                /*{
-          'html/javascripts/global.js': ['coffeescript/*.coffee'] // compile and concat into single file
-        }*/
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'html/images/',                   // Src matches are relative to this path
+                    src: ['*.{png,jpg,jpeg,gif}'],   // Actual patterns to match
+                    dest: 'html/images/compressed'                  // Destination path prefix
+                }]
             }
-        }
+        },
+        csscomb: {
+            dist: {
+                files: {
+                    'html/stylesheets/resorted-main.css': ['html/stylesheets/main.css']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: false
+            },
+            target: {
+                files: {
+                    'html/stylesheets/compressed-main.css': ['html/stylesheets/main.css']
+                }
+            }
+        },
     });
 
+    grunt.loadNpmTasks('grunt-contrib-cssmin'); // https://github.com/gruntjs/grunt-contrib-cssmin
+    grunt.loadNpmTasks('grunt-csscomb'); // https://github.com/csscomb/grunt-csscomb
     grunt.loadNpmTasks('grunt-contrib-sass'); // https://github.com/gruntjs/grunt-contrib-sass
     grunt.loadNpmTasks('grunt-postcss'); // https://github.com/postcss/autoprefixer
-    grunt.loadNpmTasks('grunt-contrib-coffee'); // https://github.com/gruntjs/grunt-contrib-coffee
-    grunt.loadNpmTasks('grunt-contrib-watch'); // https://github.com/gruntjs/grunt-contrib-watch
-    grunt.registerTask('default', ['sass:dist', 'postcss:dist', 'coffee', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-imagemin'); // https://github.com/gruntjs/grunt-contrib-imagemin
+    grunt.loadNpmTasks('grunt-contrib-watch'); // https://github.com/gruntjs/grunt-contrib-watch /// http://blog.grayghostvisuals.com/grunt/image-optimization/
+    grunt.registerTask('default', ['sass:dist', 'postcss:dist', 'csscomb:dist', 'cssmin', 'imagemin', 'watch']);
 };
